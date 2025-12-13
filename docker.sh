2ced33399ceedaf6847a42917ef63493207bf4fc
unknown <bijilinarioffical@gmail.com>
2025-12-13 18:58:40 +0530

    pusing to git

--- docker.sh
+++ docker.sh
@@ -5,3 +5,12 @@ b=2
 
 sum=$((a + b))
 echo "Sum: $sum"
+
+
+#     6  git status
+#     7  git add docker.sh 
+#     8  git status
+#     9  git commite -m "pusing to git"
+#    10  git commit -m "pusing to git"
+#    11  git status
+#    12  git push -u origin main
