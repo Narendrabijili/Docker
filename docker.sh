a818b3307684a8b75ed0118de201d73cd476a88a
narendra <bijilinarioffical@gmail.com>
2025-12-13 23:53:05 +0530

    Update docker.sh
    Removed commented git commands and added a clone command.

--- docker.sh
+++ docker.sh
@@ -6,11 +6,10 @@ b=2
 sum=$((a + b))
 echo "Sum: $sum"
 
-
-#     6  git status
-#     7  git add docker.sh 
-#     8  git status
-#     9  git commite -m "pusing to git"
-#    10  git commit -m "pusing to git"
-#    11  git status
-#    12  git push -u origin main
+#  git clone git repo link
+#   git status
+#      git add docker.sh 
+#      git status
+#      git commit -m "pusing to git"
+#      git status
+#      git push -u origin main
